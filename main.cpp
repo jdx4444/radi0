a0ec58dc74623e46c4f5d5d834af4df95621059c
Justin DeBarr <justin.debarr@aol.com>
2024-12-14 00:02:48 -0700

    Update imgui.ini and main.cpp with latest changes

--- main.cpp
+++ main.cpp
@@ -17,12 +17,8 @@
 
 const float VIRTUAL_WIDTH = 20.0f;
 const float VIRTUAL_HEIGHT = 10.0f;
-const float SCREEN_WIDTH_IN = 5.0f;
-const float SCREEN_HEIGHT_IN = 2.5f;
-const float SCREEN_DPI = 96.0f;
 
-int main(int, char**)
-{
+int main(int, char**) {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
         printf("Error: %s\n", SDL_GetError());
         return -1;
@@ -31,47 +27,50 @@ int main(int, char**)
 #if defined(IMGUI_IMPL_OPENGL_ES2)
     const char* glsl_version = "#version 100";
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS,0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION,2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION,0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #elif defined(__APPLE__)
     const char* glsl_version = "#version 150";
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION,3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION,2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 #else
     const char* glsl_version = "#version 130";
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS,0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION,3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION,0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #endif
 
 #ifdef SDL_HINT_IME_SHOW_UI
     SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
 #endif
 
-    int window_width = (int)(SCREEN_WIDTH_IN * SCREEN_DPI);
-    int window_height = (int)(SCREEN_HEIGHT_IN * SCREEN_DPI);
+    // Get the current screen resolution dynamically
+    SDL_DisplayMode DM;
+    if (SDL_GetCurrentDisplayMode(0, &DM) != 0) {
+        printf("SDL_GetCurrentDisplayMode failed: %s\n", SDL_GetError());
+        return -1;
+    }
+    int window_width = DM.w;
+    int window_height = DM.h;
 
-    float scale_x = window_width / VIRTUAL_WIDTH;
-    float scale_y = window_height / VIRTUAL_HEIGHT;
+    float scale_x = (float)window_width / VIRTUAL_WIDTH;
+    float scale_y = (float)window_height / VIRTUAL_HEIGHT;
 
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE,8);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_BORDERLESS);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_BORDERLESS | SDL_WINDOW_FULLSCREEN_DESKTOP);
     SDL_Window* window = SDL_CreateWindow("Retro Car Head Unit", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_width, window_height, window_flags);
     if (!window) {
         printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
         SDL_Quit();
         return -1;
     }
-
-    SDL_SetWindowResizable(window, SDL_FALSE);
-    SDL_SetWindowPosition(window, 0, 0);
 
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     if (!gl_context) {
@@ -85,27 +84,26 @@ int main(int, char**)
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();(void)io;
+    ImGuiIO& io = ImGui::GetIO();
     io.Fonts->AddFontDefault();
 
     ImGui::StyleColorsDark();
     ImGuiStyle& style = ImGui::GetStyle();
     style.WindowRounding = 0.0f;
-    style.WindowPadding = ImVec2(0,0);
+    style.WindowPadding = ImVec2(0, 0);
 
     ImVec4* colors = style.Colors;
-    colors[ImGuiCol_WindowBg]      = ImVec4(0,0,0,1);
-    colors[ImGuiCol_Text]          = ImVec4(0,1,0,1);
-    colors[ImGuiCol_Border]        = ImVec4(0,1,0,1);
-    colors[ImGuiCol_FrameBg]       = ImVec4(0,0,0,1);
-    colors[ImGuiCol_PlotHistogram] = ImVec4(0,1,0,1);
+    colors[ImGuiCol_WindowBg]      = ImVec4(0, 0, 0, 1);
+    colors[ImGuiCol_Text]          = ImVec4(0, 1, 0, 1);
+    colors[ImGuiCol_Border]        = ImVec4(0, 1, 0, 1);
+    colors[ImGuiCol_FrameBg]       = ImVec4(0, 0, 0, 1);
+    colors[ImGuiCol_PlotHistogram] = ImVec4(0, 1, 0, 1);
 
-    ImVec4 clear_color = ImVec4(0,0,0,1);
+    ImVec4 clear_color = ImVec4(0, 0, 0, 1);
 
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // Initialize BluetoothAudioManager in place of AudioManager
     BluetoothAudioManager audioManager;
     if (!audioManager.Initialize()) {
         printf("Failed to initialize BluetoothAudioManager.\n");
@@ -115,7 +113,6 @@ int main(int, char**)
     audioManager.AddToPlaylist("Track2.mp3", 200.0f);
     audioManager.Play();
 
-    // Initialize Sprite
     Sprite sprite;
     sprite.Initialize(scale_x, scale_y);
 
@@ -171,8 +168,8 @@ int main(int, char**)
                               ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollbar |
                               ImGuiWindowFlags_NoScrollWithMouse;
 
-        ImGui::SetNextWindowPos(ImVec2(0,0));
-        ImGui::SetNextWindowSize(ImVec2((float)window_width,(float)window_height));
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+        ImGui::SetNextWindowSize(ImVec2((float)window_width, (float)window_height));
 
         ImGui::Begin("Car Head Unit", nullptr, wf);
 
@@ -182,7 +179,7 @@ int main(int, char**)
         ImGui::End();
 
         ImGui::Render();
-        glViewport(0,0,window_width,window_height);
+        glViewport(0, 0, window_width, window_height);
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
